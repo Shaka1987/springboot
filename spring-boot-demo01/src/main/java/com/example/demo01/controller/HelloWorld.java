a0ebfb538487a5c23aa5cc1693e124eacd60ebb6
Shaka <yhpjs@sina.com.cn>
2020-04-24 23:03:19 +0800

    front-end with thymeleaf

--- spring-boot-demo01/src/main/java/com/example/demo01/controller/HelloWorld.java
+++ spring-boot-demo01/src/main/java/com/example/demo01/controller/HelloWorld.java
@@ -1,14 +1,25 @@
 package com.example.demo01.controller;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
+@Controller
 public class HelloWorld {
 	@ResponseBody()
 	@RequestMapping("/hello")
 	public String hello() {
 		return "Hello world!";
 	}
+	 @RequestMapping(value = "success", method = RequestMethod.GET)
 	
+	public String success(Map<String, Object> map) {
+		map.put("hello","<h1>你好</h1>");
+		map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
+		return "success";
+	}
 }
